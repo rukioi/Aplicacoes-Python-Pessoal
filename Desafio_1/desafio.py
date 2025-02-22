e768b5b903fdc2fcb2b7b12bf8e4ebc301a08e66
rukioi <muribeco@hotmail.com>
2025-02-21 22:26:55 -0300

    2° Atualização Programa 1

---
+++ Desafio_1/desafio.py
@@ -0,0 +1,11 @@
+import funcoes
+
+while True:
+    Nome=input("Digite o Nome:").title().lstrip()
+ 
+        
+    Telefone=input("Digite seu Telefone:")
+            
+    print(Nome)
+    break
+#funcoes.adicionar
--- Desafio_1/funcoes.py
+++ Desafio_1/funcoes.py
@@ -20,37 +20,45 @@ Branco="\033[37m"
 Negrito="\033[1m"
 Reset="\033[0m"
 
-contatos= []
+pessoas = dict()
+
+def buscar(Nome):
+    "Digite o nome da pessoa que você deseja buscar para receber todos os dados da pessoa!"
+    validacao=0
+    for i,v in pessoas.items():
+        if Nome in v:
+            validacao=1
+            print(f"Nome: {v[0]}")
+            print(f"Telefone: {v[1]}")
+            print(f"E-mail: {v[2]}")
+    if validacao==0:
+        print(f'Não encontramos "{Nome}" no nosso Sistema!')
 
 
-def cabecalho(texto=None):
-    if texto is None or texto.strip(): #Validação da função
-        print(Negrito+Vermelho+" !ERROR! Você Esqueceu de Passar Argumento!"+ Reset)
-    else:
-        print(Negrito+"-"*35) #Código cabeçalho 
-        print(Ciano + texto.center(35) + Reset)
-        print(Negrito+"-"*35 + Reset)
-    
+
 def adicionar(Nome="Não Informado",Telefone="Não Informado",Email="Não Informado"):
     contato= list()
-    pessoa= dict()
 
     contato.append(Nome)
     contato.append(Telefone)
     contato.append(Email)
     
-    pessoa[f'Pessoa {len(contatos)+1}']=contato
-
-    contatos.append(pessoa)
+    pessoas[f'Pessoa {len(pessoas)+1}']=contato
+    
+adicionar("Murilo Souza de Barros","79991463940","muribeco@hotmail.com")
+adicionar("Maria Bragantina Silva","79999816647","mariabraga@gmail.com")
+adicionar("José de Augusto Carvalho","79325916647","josecarv@gmail.com")
 
 def listar():
-    for i in contatos:
-        print(f"Nome: {i}")
+    print(Negrito+"-"*70+Reset)
+    print(Negrito+Ciano+"Lista de Pessoas".center(70)+Reset)
+    print(Negrito+"-"*70+Reset)
 
-adicionar("Murilo","79991463940","MURIBECO@HOTMAIL.COM")
-adicionar("Maria","79999816647","maria@gmail.com")
+    for pessoa, dados in pessoas.items():
+        print(f"{dados[0].ljust(30)}{dados[1].ljust(18)}{dados[2].ljust(15)}")
 
-listar()
-print(contatos)
 
+#listar()
+buscar("Murilo Souza de Barros")
 #Problemas em Tirar saída, Tava com a ideia de Fazer somente um Dicionário e pegar do dicionário, sem precisar fazer essa lista contatos
+#validar_nomes#()
